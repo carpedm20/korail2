88b2dfb2dea8519fc04ced4f4f785cc737177016
Taehoon Kim <carpedm20@gmail.com>
2014-08-25 14:37:27 +0900

    readme.rst encoding fix

--- setup.py
+++ setup.py
@@ -36,38 +36,41 @@ with open('korail2/__init__.py') as f:
     version = re.search(r'__version__\s*=\s*\'(.+?)\'', f.read()).group(1)
 assert version
 
-setup(
-    name='korail2',
-    packages=['korail2'],
-    version=version,
-    description='Korail(www.letskorail.com) wrapper for Python',
-    long_description=open('README.rst').read(),
-    license='BSD License',
-    author='Taehoon Kim',
-    author_email='carpedm20@gmail.com',
-    url='http://github.com/carpedm20/korail2',
-    keywords=['Korail'],
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
-    install_requires=[
-        'requests',
-        'enum34',
-    ],
-)
+import codecs
+
+with codecs.open('README.rst','r',encoding='utf8') as f:
+    setup(
+        name='korail2',
+        packages=['korail2'],
+        version=version,
+        description='Korail(www.letskorail.com) wrapper for Python',
+        long_description=f.read(),
+        license='BSD License',
+        author='Taehoon Kim',
+        author_email='carpedm20@gmail.com',
+        url='http://github.com/carpedm20/korail2',
+        keywords=['Korail'],
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.1',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Programming Language :: Python :: Implementation :: PyPy',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        ],
+        install_requires=[
+            'requests',
+            'enum34',
+        ],
+    )
