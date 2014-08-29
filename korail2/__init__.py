d9cf794285b76f84980b51e81cc9b8c7b62de2a6
sng2c <sng2nara@gmail.com>
2014-08-29 10:59:59 +0900

    update import

--- korail2/__init__.py
+++ korail2/__init__.py
@@ -8,8 +8,8 @@
     :copyright: (c) 2014 by Taehoon Kim.
     :license: BSD, see LICENSE for more details.
 """
-from korail2 import Korail, Passenger, AdultPassenger, ChildPassenger, SeniorPassenger, TrainType, ReserveOption
-from korail2 import KorailError, NeedToLoginError, SoldOutError, NoResultsError
+from .korail2 import Korail, Passenger, AdultPassenger, ChildPassenger, SeniorPassenger, TrainType, ReserveOption
+from .korail2 import KorailError, NeedToLoginError, SoldOutError, NoResultsError
 
 __version__ = '0.0.6'
 __all__ = ['Korail', 'Passenger', 'AdultPassenger', 'ChildPassenger', 'SeniorPassenger', 'TrainType', 'ReserveOption',
