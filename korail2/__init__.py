bc29cad62e46d649a8f1298af427eef4baf21379
Taehoon Kim <carpedm20@gmail.com>
2017-04-22 00:01:08 +0900

    version up to 0.2.7 thanks to @bulzipke #25

--- korail2/__init__.py
+++ korail2/__init__.py
@@ -11,6 +11,5 @@
 from .korail2 import Korail, Passenger, AdultPassenger, ChildPassenger, SeniorPassenger, TrainType, ReserveOption
 from .korail2 import KorailError, NeedToLoginError, SoldOutError, NoResultsError
 
-__version__ = '0.0.6'
 __all__ = ['Korail', 'Passenger', 'AdultPassenger', 'ChildPassenger', 'SeniorPassenger', 'TrainType', 'ReserveOption',
            'KorailError', 'NeedToLoginError', 'SoldOutError', 'NoResultsError']
--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ except ImportError:
     from setuptools import setup
 
 # detect the current version
-version = '0.2.6'
+version = '0.2.7'
 
 import codecs
 
