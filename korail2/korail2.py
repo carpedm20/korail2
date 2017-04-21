f0aa69f54741c3459fa39bc59b1e9087c013814c
bulzipke <bulzipke@naver.com>
2017-04-21 18:56:47 +0900

    Remove bug
    My mistake, remove unicode_literals.

--- korail2/korail2.py
+++ korail2/korail2.py
@@ -6,7 +6,6 @@
     :copyright: (c) 2014 by Taehoon Kim.
     :license: BSD, see LICENSE for more details.
 """
-from __future__ import unicode_literals
 import re
 import requests
 import itertools
