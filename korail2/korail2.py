db881d103eef0ef480c8f541f319df89d34ac5de
hongs.lee <hongsworld@users.noreply.github.com>
2022-09-08 11:46:25 +0900

    remove blank line

--- korail2/korail2.py
+++ korail2/korail2.py
@@ -589,7 +589,6 @@ When you want change ID using existing object,
 
         r = self._session.post(url, data=data)
         j = json.loads(r.text)
-
         if j['strResult'] == 'SUCC' and j.get('strMbCrdNo') is not None:
             self._key = j['Key']
             self.membership_number = j['strMbCrdNo']
